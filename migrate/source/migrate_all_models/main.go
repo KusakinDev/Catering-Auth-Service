ba0b53ee02288764fa8f214267ca402469463d1d
alexander kusakin <pm12.kusakin@gmail.com>
2025-04-07 17:20:10 +0700

    ./migrate/: add seeding in main migrate

--- migrate/source/migrate_all_models/main.go
+++ migrate/source/migrate_all_models/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/KusakinDev/Catering-Auth-Service/internal/database"
 	accountmodel "github.com/KusakinDev/Catering-Auth-Service/internal/models/account_model"
 	resetpasswordmodel "github.com/KusakinDev/Catering-Auth-Service/internal/models/reset_password_model"
@@ -20,6 +22,23 @@ func main() {
 
 	var resetCode resetpasswordmodel.ResetCode
 	resetCode.MigrateToDB(db)
+
+	sqlStatements := []string{
+		`INSERT INTO roles (role, role_string) VALUES
+        ('adm', 'Администратор'),
+        ('mng', 'Менеджер'),
+        ('wtr', 'Официант'),
+        ('ktn', 'Кухня'),
+        ('bar', 'Бар');`,
+	}
+
+	for _, stmt := range sqlStatements {
+		if err := db.Connection.Exec(stmt).Error; err != nil {
+			log.Println("Error executing seed: ", stmt, err)
+		}
+	}
+
+	log.Println("Success seeding")
 
 	db.CloseDB()
 }

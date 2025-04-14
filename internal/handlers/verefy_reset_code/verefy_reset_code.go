bcd606ade77756e26e97c0c16f17cd9eb1a6383f
alexander kusakin <pm12.kusakin@gmail.com>
2025-04-14 17:13:58 +0700

    ./internal/handlers/verefy_reset_code/: add save new passord in table

--- internal/handlers/verefy_reset_code/verefy_reset_code.go
+++ internal/handlers/verefy_reset_code/verefy_reset_code.go
@@ -13,9 +13,11 @@ func VerefyResetCodeHandle(c *gin.Context) (int, string, string, string) {
 	var resetFormFront resetpasswordcode.ResetCode
 	var resetFormDB resetpasswordcode.ResetCode
 	var user useraccount.UserAccount
+	var userFront useraccount.UserAccount
 	var err error
 
 	resetFormFront.DecodeFromContext(c)
+	userFront = resetFormFront.User
 	user = resetFormFront.User
 
 	err = user.GetFromTableByEmail()
@@ -23,7 +25,7 @@ func VerefyResetCodeHandle(c *gin.Context) (int, string, string, string) {
 		return 404, "", "", "User not found"
 	}
 
-	resetFormDB.Id_user = user.Id
+	resetFormDB.UserId = user.Id
 
 	err = resetFormDB.GetFromTableByUserId()
 	if err != nil {
@@ -52,6 +54,10 @@ func VerefyResetCodeHandle(c *gin.Context) (int, string, string, string) {
 	}
 
 	resetFormDB.DeleteFromTableByCode()
+
+	user.SetPasswordHash(userFront.Password)
+
+	user.UpdateInTable()
 
 	return 200, accessToken, refreshToken, "Reset password is success"
 
